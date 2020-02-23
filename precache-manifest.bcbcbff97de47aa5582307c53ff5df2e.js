f382d9b07bc19d8992a8831dd311ba4aeb77ebae
Karina <47184215+KarinaSakuta@users.noreply.github.com>
2020-02-23 16:28:42 +0300

    Updates

---
+++ precache-manifest.bcbcbff97de47aa5582307c53ff5df2e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11eb5fc2e54e55e6d78709e45824f354",
+    "url": "/ToDo/index.html"
+  },
+  {
+    "revision": "5f50b39b32774cb93bd3",
+    "url": "/ToDo/static/css/main.47cd3007.chunk.css"
+  },
+  {
+    "revision": "e7991df177bd86907d73",
+    "url": "/ToDo/static/js/2.0152dbd1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ToDo/static/js/2.0152dbd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f50b39b32774cb93bd3",
+    "url": "/ToDo/static/js/main.58082e93.chunk.js"
+  },
+  {
+    "revision": "b00bcfdc695a4a69eba7",
+    "url": "/ToDo/static/js/runtime-main.c00168ed.js"
+  },
+  {
+    "revision": "5d5c235afcf4c5db7784def0d20f0a68",
+    "url": "/ToDo/static/media/cat.5d5c235a.png"
+  },
+  {
+    "revision": "30e14625cff9795e1a2b3674898a05fa",
+    "url": "/ToDo/static/media/icon.30e14625.png"
+  }
+]);
